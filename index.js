ef7a3171f8d9fb6326ee0a5b4bd406a14bc10a84
Luiz Picanço <lpicanco@gmail.com>
2019-12-12 23:01:34 -0300

    Clean code

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const extractFrames = require('ffmpeg-extract-frames')
+const extractFrames = require('ffmpeg-extract-frames');
 const path = require('path');
 const express = require('express');
 const uuidv4 = require('uuid/v4');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
     offsetInMillis = req.query["offset-in-millis"]
     url = req.query.url
     fileName = path.join(__dirname, "./imgs_" + uuidv4() + ".jpg");
-
-    console.info("url: " + url)
-    console.info("offSet: " + offsetInMillis)
-    console.info("fileName: " + fileName)
-
 
     await extractFrames({
         input: url,
@@ -26,4 +21,4 @@ app.get('/', async (req, res) => {
     res.sendFile(fileName);
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
